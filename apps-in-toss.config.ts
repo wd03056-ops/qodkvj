4cc83d56cd9645ab0ff612a9b3b648c0918a0d4a
wd03056-ops <wd03056@gmail.com>
2026-08-15 13:55:01 +0900

    호호

--- apps-in-toss.config.ts
+++ apps-in-toss.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from '@apps-in-toss/web-framework/config';
 
 export default defineConfig({
-  appName: 'qodkvj',
+  appName: 'qodkvk',
   brand: {
     primaryColor: '#3182F6',
   },
